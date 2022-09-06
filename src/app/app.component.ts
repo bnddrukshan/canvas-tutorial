70e1133b1e9b988cbcb78b752939ae8d7d1b1881
Dhanushka Rukshan <bnddrukshan@gmail.com>
2022-09-06 21:26:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  context?: CanvasRenderingContext2D;
+
+  @ViewChild("mycanvas") mycanvas: any;
+
   title = 'canvas-tutorial';
+
+  ngOnInit(): void {
+    let canvas = this.mycanvas.nativeElement;
+    let context = canvas.getContext('2d');
+    context.clearRect(0,0,300,300);
+  }
+ 
 }
