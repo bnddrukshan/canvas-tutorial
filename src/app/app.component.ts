68cf85daed6667e9974f963e851815c829cb6c61
Dhanushka Rukshan <bnddrukshan@gmail.com>
2022-09-06 21:55:15 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,21 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class AppComponent implements OnInit {
   context?: CanvasRenderingContext2D;
 
-  @ViewChild("mycanvas") mycanvas: any;
-
   title = 'canvas-tutorial';
 
   ngOnInit(): void {
-    let canvas = this.mycanvas.nativeElement;
-    let context = canvas.getContext('2d');
-    context.clearRect(0,0,300,300);
+    let canvas =  <HTMLCanvasElement> document.getElementById("canvas");
+    let ctx = canvas?.getContext('2d');
+    var img = new Image();
+    if(ctx) {
+      debugger;
+      //context.clearRect(0,0,300,300);
+      ctx.fillStyle = "green";
+      ctx.fillRect(0, 0, 900, 900);
+    }
+    
+    //context?.drawImage(img, 0, 0);
+    //img.src = ".././assets/images/f097075ec37cdbf9e77ef4887dc22a6e.jpg";
   }
  
 }
